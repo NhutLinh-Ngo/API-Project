2caeeed838b10a17c88042e910f056674dcd7517
NhutLinh-Ngo <nngo6133@gmail.com>
2022-09-26 14:35:50 -0700

    add get all of current user bookings

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -29,7 +29,10 @@ module.exports = (sequelize, DataTypes) => {
 			},
 			endDate: {
 				type: DataTypes.DATEONLY,
-				allowNull: false
+				allowNull: false,
+				validate: {
+					isAfter: this.startDate
+				}
 			}
 		},
 		{
