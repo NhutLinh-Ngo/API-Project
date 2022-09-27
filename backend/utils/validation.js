4b209bd4ca1075f38a812c778709dafffb20306f
NhutLinh-Ngo <nngo6133@gmail.com>
2022-09-27 12:41:17 -0700

    remove all try catch and update errors handlers

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,16 +1,20 @@
 // backend/utils/validation.js
 const { validationResult } = require('express-validator');
-
+const { check } = require('express-validator');
 // middleware for formatting errors from express-validator middleware
 // (to customize, see express-validator's documentation)
+
 const handleValidationErrors = (req, _res, next) => {
 	const validationErrors = validationResult(req);
-
+	console.log(validationErrors);
 	if (!validationErrors.isEmpty()) {
-		const errors = validationErrors.array().map((error) => `${error.msg}`);
+		const erObj = {};
+		const errors = validationErrors
+			.array()
+			.map((error) => (erObj[error.param] = `${error.msg}`));
 
-		const err = Error('Bad request.');
-		err.errors = errors;
+		const err = Error('Validation Error');
+		err.errors = erObj;
 		err.status = 400;
 		err.title = 'Bad request.';
 		next(err);
@@ -18,6 +22,39 @@ const handleValidationErrors = (req, _res, next) => {
 	next();
 };
 
-module.exports = {
+const validateSpotImage = [
+	check('url')
+		.exists({ checkFalsy: true })
+		.withMessage('url for image required'),
 	handleValidationErrors
+];
+const validateSpot = [
+	check('address')
+		.exists({ checkFalsy: true })
+		.withMessage('Street address is required'),
+	check('city').exists({ checkFalsy: true }).withMessage('City is required'),
+	check('state').exists({ checkFalsy: true }).withMessage('State is required'),
+	check('country')
+		.exists({ checkFalsy: true })
+		.withMessage('Country is required'),
+	check('name')
+		.exists({ checkFalsy: true })
+		.withMessage('Name is required')
+		.bail()
+		.isLength({ max: 50 })
+		.withMessage('Name must be less than 50 characters')
+		.bail()
+		.notEmpty()
+		.withMessage('Name must be more than 1 characters'),
+	check('description')
+		.exists({ checkFalsy: true })
+		.withMessage('Description is required'),
+	check('price').exists().withMessage('Price per day is required'),
+	handleValidationErrors
+];
+
+module.exports = {
+	handleValidationErrors,
+	validateSpot,
+	validateSpotImage
 };
