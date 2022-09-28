4a4000cc410d09076d82cc7d6002b4f0c3a08056
NhutLinh-Ngo <nngo6133@gmail.com>
2022-09-28 08:43:45 -0700

    moved user validation into its own file for export

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -98,10 +98,36 @@ const validateBookingDates = [
 	check('endDate').isDate().withMessage('must be date YYYY-MM-DD format'),
 	handleValidationErrors
 ];
+
+const validateSignup = [
+	check('email')
+		.exists({ checkFalsy: true })
+		.isEmail()
+		.withMessage('Please provide a valid email.'),
+	check('username')
+		.exists({ checkFalsy: true })
+		.isLength({ min: 4 })
+		.withMessage('Please provide a username with at least 4 characters.'),
+	check('username').not().isEmail().withMessage('Username cannot be an email.'),
+	check('password')
+		.exists({ checkFalsy: true })
+		.isLength({ min: 6 })
+		.withMessage('Password must be 6 characters or more.'),
+	check('firstName')
+		.exists({ checkFalsy: true })
+		.isLength({ min: 1 })
+		.withMessage('Please provide a first name with at least 1 characters.'),
+	check('lastName')
+		.exists({ checkFalsy: true })
+		.isLength({ min: 1 })
+		.withMessage('Please provide a last name with at least 1 characters.'),
+	handleValidationErrors
+];
 module.exports = {
 	handleValidationErrors,
 	validateSpot,
 	validateSpotImage,
 	validateQueryParams,
-	validateBookingDates
+	validateBookingDates,
+	validateSignup
 };
